18a15f4a9f97c698205ed947b0ed2cbf840d6b9e
Vuthanhcong <vuthanhcong96@gmail.com>
2020-04-21 15:18:17 +0700

    Build test commit

---
+++ TestGitHub/Assets/V1.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class V1 : MonoBehaviour
+{
+    [System.NonSerialized] public GameObject v2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        v2?.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
